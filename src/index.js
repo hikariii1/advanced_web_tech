02f1cd561d14c4c4362cc730560d2336ae6dcc33
hikariii1 <f.kuanova1@gmail.com>
2025-02-04 23:33:59 +0600

    step 13 Preparing for Todo List Project status filter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
+
 const App = () => {
   const todoData = [
-    { label: 'Drink Coffee', important: false,  id: 1},
-        { label: 'Implement React Application', important: true, id: 2 },
-        { label: 'Make notes from your stud', important: false, id: 3 },
-        { label: 'Do not drink alchohol', important: false, id: 4 },
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Implement React Application', important: true, id: 2 },
+    { label: 'Make notes from your stud', important: false, id: 3 },
+    { label: 'Do not drink alchohol', important: false, id: 4 },
   ];
   return (
     <div>
@@ -17,6 +19,7 @@ const App = () => {
       <AppHeader />
       <SearchPanel />
       <TodoList todos={todoData} />
+      <ItemStatusFilter />
     </div>
   );
 }
