f58eddb3b25a29bfa2f7d540efeb63ed308af572
hikariii1 <f.kuanova1@gmail.com>
2025-02-04 23:13:18 +0600

    step 10 Spead operator for arrays

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,16 +4,14 @@ const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
         return (
             <li>
-                <TodoListItem
-                    label={item.label}
-                    important={item.important} />
+                <TodoListItem {...item} />
+
             </li>
         );
     });
-
     return (
         <ul>
-            { elements }
+            {elements}
         </ul>
     );
 };
